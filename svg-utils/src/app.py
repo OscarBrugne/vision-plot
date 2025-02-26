8e693d28eedfc170f641a6c77052ecf21c183c25
OscarBrugne <oscar.brugne@tum.de>
2025-02-26 10:47:20 +0100

    Refactor PathToSVGController into a class.
    - Encapsulated PathToSVGController in a class for better structure.
    - Updated app.py to use the new controller structure.
    - Removed two redundant lines in PathToSVGService.

--- svg-utils/src/app.py
+++ svg-utils/src/app.py
@@ -1,9 +1,10 @@
 from bottle import Bottle, run
-from controllers.path_to_svg_controller import path_to_svg_controller
+from controllers.path_to_svg_controller import PathToSVGController
 
 app: Bottle = Bottle()
 
-app.mount("/svg", path_to_svg_controller)
+path_to_svg_controller = PathToSVGController()
+app.mount("/svg", path_to_svg_controller.app)
 
 if __name__ == "__main__":
     run(app, host="localhost", port=8080)
