aa1881a3a2363baac1d585d7665fcdfc66f13239
OscarBrugne <oscar.brugne@tum.de>
2025-02-27 17:36:44 +0100

    Use environment variables for host and port
    - Replaced hardcoded host and port with HOST and PORT environment variables to separate development and deployment configurations.
    - Added a check to ensure these variables are set.

--- svg-utils/svg_utils/app.py
+++ svg-utils/svg_utils/app.py
@@ -1,3 +1,5 @@
+import os
+
 from bottle import Bottle, run
 from controllers import PathToSVGController
 
@@ -7,4 +9,9 @@ path_to_svg_controller = PathToSVGController()
 app.mount("/svg", path_to_svg_controller.app)
 
 if __name__ == "__main__":
-    run(app, host="localhost", port=8080)
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+    if not host or not port:
+        raise ValueError("Environment variables HOST and PORT must be set.")
+
+    run(app, host=host, port=port)
