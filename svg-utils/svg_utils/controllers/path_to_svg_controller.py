cfda45cc163928d777d9bcb34e48d61f6f316d3c
OscarBrugne <oscar.brugne@tum.de>
2025-03-06 14:08:34 +0100

    Use Pydantic for validation and switch to form-urlencoded
    - Introduced Pydantic models (SinglePathRequest and MultiplePathsRequest) for validating request data.
    - Replaced manual validation with Pydantic validation and added detailed error messages.
    - Changed request content type handling from application/json to application/x-www-form-urlencoded.

--- svg-utils/svg_utils/controllers/path_to_svg_controller.py
+++ svg-utils/svg_utils/controllers/path_to_svg_controller.py
@@ -1,7 +1,51 @@
+import json
 from typing import Any, Dict, List, Optional, Tuple
 
 from bottle import Bottle, request, response
+from pydantic import BaseModel, ValidationError, field_validator
 from services import PathToSVGService
+
+
+class SinglePathRequest(BaseModel):
+    """Request schema for a single path request."""
+
+    points: List[List[int]]
+    size: List[int]
+    viewbox: Optional[List[int]] = None
+    is_closed_path: bool = False
+    stroke: str = "black"
+    stroke_width: int = 1
+
+    @field_validator("*", mode="before")
+    @classmethod
+    def parse_json_field(cls, value: str) -> Any:
+        try:
+            return json.loads(value)
+        except json.JSONDecodeError as e:
+            raise ValueError(
+                f"Invalid JSON value: '{value}' is not a valid JSON format."
+            ) from e
+
+
+class MultiplePathsRequest(BaseModel):
+    """Request schema for a multiple paths request."""
+
+    paths: List[List[List[int]]]
+    size: List[int]
+    viewbox: Optional[List[int]] = None
+    is_closed_path: bool = False
+    stroke: str = "black"
+    stroke_width: int = 1
+
+    @field_validator("*", mode="before")
+    @classmethod
+    def parse_json_field(cls, value: str) -> Any:
+        try:
+            return json.loads(value)
+        except json.JSONDecodeError as e:
+            raise ValueError(
+                f"Invalid JSON value: '{value}' is not a valid JSON format."
+            ) from e
 
 
 class PathToSVGController:
@@ -11,6 +55,7 @@ class PathToSVGController:
         """Initialize a new PathToSVGController object."""
         self._app: Bottle = Bottle()
         self._svg_service: PathToSVGService = PathToSVGService()
+
         self._register_routes()
 
     @property
@@ -24,19 +69,29 @@ class PathToSVGController:
         return self._app
 
     def _register_routes(self) -> None:
-        """
-        Register routes for the controller.
-        """
+        """Register routes for the controller."""
         self._app.post("/generate-single-path", callback=self.generate_single_path)
         self._app.post(
             "/generate-multiple-paths", callback=self.generate_multiple_paths
         )
 
+    def _generate_validation_error_message(self, e: ValidationError) -> str:
+        errors = e.errors()
+        error_msgs = []
+        for error in errors:
+            key = error["loc"][0]
+            value = error["input"]
+            msg = error["msg"]
+            error_msgs.append(f"The parameter '{key}:{value}' is invalid: {msg}")
+        error_message = "\n ".join(error_msgs)
+
+        return error_message
+
     def generate_single_path(self) -> Dict[str, str]:
         """
         Handle a POST request to generate an SVG with a single path defined by a set of points.
 
-        Expects a JSON payload with the following structure:
+        Expects a form-urlencoded payload with the following fields:
             {
                 "points": [[x1, y1], [x2, y2], ...],
                 "size": [width, height],
@@ -49,25 +104,29 @@ class PathToSVGController:
         Returns:
             Dict[str, str]: A JSON response with the SVG string mapped to the key "svg".
         """
-        data: Dict[str, Any] = request.json
-
-        if "points" not in data:
+        if request.content_type != "application/x-www-form-urlencoded":
             response.status = 400
-            return {"error": "Missing required field: 'points'."}
-        if "size" not in data:
+            return {
+                "error": "Invalid Content-Type",
+                "message": "Expected application/x-www-form-urlencoded",
+            }
+
+        try:
+            data = SinglePathRequest.model_validate(request.forms)
+        except ValidationError as e:
             response.status = 400
-            return {"error": "Missing required field: 'size'."}
-
-        points: List[Tuple[int, int]] = data["points"]
-        size: Tuple[int, int] = tuple(data["size"])
-
-        viewbox: Optional[Tuple[int, int, int, int]] = data.get("viewbox", None)
-        is_closed_path: bool = data.get("is_closed_path", False)
-        stroke: str = data.get("stroke", "black")
-        stroke_width: int = data.get("stroke_width", 1)
+            return {
+                "error": "Invalid request data",
+                "message": self._generate_validation_error_message(e),
+            }
 
         svg_string: str = self._svg_service.generate_line_path_svg(
-            points, size, viewbox, is_closed_path, stroke, stroke_width
+            data.points,
+            data.size,
+            data.viewbox,
+            data.is_closed_path,
+            data.stroke,
+            data.stroke_width,
         )
 
         response.content_type = "application/json"
@@ -77,7 +136,7 @@ class PathToSVGController:
         """
         Handle a POST request to generate an SVG with multiple paths defined by sets of points.
 
-        Expects a JSON payload with the following structure:
+        Expects a form-urlencoded payload with the following fields:
             {
                 "paths": [[[x1, y1], [x2, y2], ...], [[x1, y1], [x2, y2], ...], ...],
                 "size": [width, height],
@@ -90,25 +149,30 @@ class PathToSVGController:
         Returns:
             Dict[str, str]: A JSON response with the SVG string mapped to the key "svg".
         """
-        data: Dict[str, Any] = request.json
-
-        if "paths" not in data:
+        if request.content_type != "application/x-www-form-urlencoded":
             response.status = 400
-            return {"error": "Missing required field: 'paths'."}
-        if "size" not in data:
+            return {
+                "error": "Invalid Content-Type",
+                "message": "Expected application/x-www-form-urlencoded",
+            }
+
+        try:
+            data = MultiplePathsRequest.model_validate(request.forms)
+            print(data.model_dump())
+        except Exception as e:
             response.status = 400
-            return {"error": "Missing required field: 'size'."}
-
-        paths: List[List[Tuple[int, int]]] = data["paths"]
-        size: Tuple[int, int] = tuple(data["size"])
-
-        viewbox: Optional[Tuple[int, int, int, int]] = data.get("viewbox", None)
-        is_closed_path: bool = data.get("is_closed_path", False)
-        stroke: str = data.get("stroke", "black")
-        stroke_width: int = data.get("stroke_width", 1)
+            return {
+                "error": "Invalid request data",
+                "message": self._generate_validation_error_message(e),
+            }
 
         svg_string: str = self._svg_service.generate_multiple_line_paths_svg(
-            paths, size, viewbox, is_closed_path, stroke, stroke_width
+            data.paths,
+            data.size,
+            data.viewbox,
+            data.is_closed_path,
+            data.stroke,
+            data.stroke_width,
         )
 
         response.content_type = "application/json"
